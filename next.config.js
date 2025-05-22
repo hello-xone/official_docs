aaffb4cb3552d12be4f9fc84b86c3eb76a651cad
Mangosago <mangosago.liu@gmail.com>
2025-05-22 18:34:39 +0700

    Add BVI Docs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextra = require('nextra');
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  latex: true
+  latex: {
+    renderer: 'katex',
+    options: {
+      macros: {
+        '\\RR': '\\mathbb{R}'
+      }
+    }
+  }
 });
 
 module.exports = withNextra({
