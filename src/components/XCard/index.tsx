d7e189f9f607e953a5ff4640318da27dbff34a6c
lucky <itmumuya@gmail.com>
2025-11-19 11:48:09 +0800

    feat：update-upgrade

---
+++ src/components/XCard/index.tsx
@@ -0,0 +1,17 @@
+'use client'
+export {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Image,
+  Link,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from '@nextui-org/react'
